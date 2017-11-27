b2a92eba1af0b505ac33aaca92b868aa6e436e7b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-11-27 16:44:11 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- specs/domain/entity/customer.spec.php
+++ specs/domain/entity/customer.spec.php
@@ -1,9 +1,12 @@
 <?php
 use Mmanager\Domain\Entity\Customer;
 
-describe('Customer', function() {
-	describe('->setName($name)', function() {
-		it('should set the customer name', function() {
+describe('Customer', function()
+{
+	describe('->setName($name)', function()
+	{
+		it('should set the customer name', function()
+		{
 			$customer = new Customer();
 			$customer->setName('Eric Claver AKAFFOU');
 			expect($customer->getName())->to->have->string('AKAFFOU');
