40a0437c18b9f5cd99bee3ee636a05cd7d60ead2
myEric <akaffou.eric@me.com>
2017-11-28 17:00:35 +0100

    Update

--- peridot.php
+++ peridot.php
@@ -1,10 +1,7 @@
 <?php
 use Evenement\EventEmitterInterface;
-use Peridot\Plugin\Prophecy\ProphecyPlugin;
 use Peridot\Plugin\Watcher\WatcherPlugin;
+
 return function(EventEmitterInterface $emitter) {
     $watcher = new WatcherPlugin($emitter);
-    $watcher->track(__DIR__ . '/src');
-    $watcher->track(__DIR__ . '/module');
-    new ProphecyPlugin($emitter);
 };
