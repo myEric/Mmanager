1e644d5cdac057761c46de3aadd743612ddba811
myEric <akaffou.eric@me.com>
2017-12-02 07:55:32 +0100

    Update

--- peridot.php
+++ peridot.php
@@ -2,8 +2,6 @@
 use Evenement\EventEmitterInterface;
 use Peridot\Plugin\Watcher\WatcherPlugin;
 
-return function(EventEmitterInterface $emitter) {
-    new WatcherPlugin($emitter);
 return function(EventEmitterInterface $emitter)
 {
 	$watcher = new WatcherPlugin($emitter);
